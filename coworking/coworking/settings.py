6e88672858e77dd4413472a90533872d82843736
ovsyannikovas <s.ovsyannikova7@gmail.com>
2022-11-06 20:48:06 +0300

    Добавлена авторизация по email, класс авторизации в backends.py

--- coworking/coworking/settings.py
+++ coworking/coworking/settings.py
@@ -124,6 +124,11 @@ STATICFILES_DIRS = []
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+AUTHENTICATION_BACKENDS = (
+    'coworking.backends.EmailBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
