e9077c622d303a6acde4a9b6a8f6a65affd51f71
ovsyannikovas <s.ovsyannikova7@gmail.com>
2022-12-21 21:43:44 +0300

    Поправлены стили для кнопок на странице мероприятий

--- coworking/account/views.py
+++ coworking/account/views.py
@@ -16,28 +16,28 @@ from event.utils import is_enrolled
 @login_required
 def personal_account(request):
     events = EventList.objects.filter(user=request.user)
-    # events_number = events.count()
-    # events_per_row = 2
+    events_number = events.count()
+    events_per_row = 2
     # cow_inacc = Coworking.objects.exclude(user=request.user)
-    # cow_booked = Coworking.objects.filter(user=request.user)
-    # cow_signs_number = cow_booked.count()
-    # cow_signs_per_row = 2
-    # if (events_number / events_per_row) % 1 != 0:
-    #     rows_number = events_number // events_per_row + 1
-    # else:
-    #     rows_number = events_number // events_per_row
-    #
-    # if (cow_signs_number / cow_signs_per_row) % 1 != 0:
-    #     cow_rows_number = cow_signs_number // cow_signs_per_row + 1
-    # else:
-    #     cow_rows_number = cow_signs_number // cow_signs_per_row
+    cow_booked = Coworking.objects.filter(user=request.user)
+    cow_signs_number = cow_booked.count()
+    cow_signs_per_row = 2
+    if (events_number / events_per_row) % 1 != 0:
+        rows_number = events_number // events_per_row + 1
+    else:
+        rows_number = events_number // events_per_row
 
-    # rows_number = events_number // events_per_row + (events_number % events_per_row)
+    if (cow_signs_number / cow_signs_per_row) % 1 != 0:
+        cow_rows_number = cow_signs_number // cow_signs_per_row + 1
+    else:
+        cow_rows_number = cow_signs_number // cow_signs_per_row
+
+    rows_number = events_number // events_per_row + (events_number % events_per_row)
     context = {
         'events': events,
-        # 'events_per_row': events_per_row,
-        # 'rows_range': range(1, rows_number),
-        # 'on_row_range': range(1, events_per_row + 1),
+        'events_per_row': events_per_row,
+        'rows_range': range(1, rows_number),
+        'on_row_range': range(1, events_per_row + 1),
     }
     return render(request, 'account/MemberPersAcc_t.html', context=context)
 
